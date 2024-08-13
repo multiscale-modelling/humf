221dc9280866adefe23ab93369ce5ee57f077091
Niklas Kappel <niklas.kappel@kit.edu>
2024-08-13 15:52:00 +0200

    Add selected files from hardcoded water model branch

---
+++ src/humf/models/force_field.py
@@ -0,0 +1,80 @@
+# pyright: reportIncompatibleMethodOverride=false
+
+import lightning as L
+import torch
+from torch import Tensor
+from torch.nn import Module
+
+
+class ForceField(L.LightningModule):
+    def __init__(
+        self,
+        energy_model: Module,
+        learning_rate: float,
+        trade_off: float,
+    ):
+        super().__init__()
+        self.save_hyperparameters(ignore=["energy_model"])
+        self.energy_model = energy_model
+        self.learning_rate = learning_rate
+        self.trade_off = trade_off
+
+    def forward(self, batch) -> tuple[Tensor, Tensor]:
+        batch.pos.requires_grad_(True)
+
+        energy = self.energy_model(batch)
+
+        grad_outputs: list[Tensor | None] = [torch.ones_like(energy)]
+        forces = torch.autograd.grad(
+            [energy],
+            [batch.pos],
+            grad_outputs=grad_outputs,  # type: ignore
+            create_graph=True,
+        )[0]
+        assert forces is not None
+        forces = -forces
+
+        return energy, forces
+
+    def training_step(self, batch):
+        metrics = self._common_step(batch)
+        metrics = {f"train/{k}": v for k, v in metrics.items()}
+        self.log_dict(metrics, batch_size=batch.batch_size)
+        return metrics["train/loss"]
+
+    @torch.enable_grad()
+    def validation_step(self, batch):
+        metrics = self._common_step(batch)
+        metrics = {f"val/{k}": v for k, v in metrics.items()}
+        self.log_dict(metrics, batch_size=batch.batch_size)
+        return metrics["val/loss"]
+
+    @torch.enable_grad()
+    def test_step(self, batch):
+        metrics = self._common_step(batch)
+        metrics = {f"test/{k}": v for k, v in metrics.items()}
+        self.log_dict(metrics, batch_size=batch.batch_size)
+        return metrics["test/loss"]
+
+    def _common_step(self, batch):
+        predicted_energy, predicted_forces = self(batch)
+        target_energy, target_forces = batch.y, batch.x
+        # Divide energy MSE loss by batch size.
+        loss_energy = torch.nn.functional.mse_loss(
+            predicted_energy, target_energy, reduction="mean"
+        )
+        # Divide forces MSE loss by number of atoms in batch.
+        loss_forces = torch.nn.functional.mse_loss(
+            predicted_forces, target_forces, reduction="sum"
+        ) / predicted_forces.size(0)
+        loss = loss_energy * self.trade_off + loss_forces * (1 - self.trade_off)
+        metrics = {
+            "loss": loss,
+            "loss_energy": loss_energy,
+            "loss_forces": loss_forces,
+        }
+        return metrics
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
---
+++ src/humf/transforms/radius_graph_pbc.py
@@ -0,0 +1,24 @@
+import numpy as np
+import torch
+from ase import Atoms
+from matscipy.neighbours import neighbour_list
+from torch_geometric.data import Data
+from torch_geometric.transforms import BaseTransform
+
+
+class RadiusGraphPBC(BaseTransform):
+    def __init__(self, cutoff, cell=None, pbc=None):
+        self.cutoff = cutoff
+        self.cell = cell
+        self.pbc = pbc
+
+    def forward(self, data: Data) -> Data:
+        assert data.pos is not None
+        cell = self.cell if self.cell is not None else data.cell
+        pbc = self.pbc if self.pbc is not None else data.pbc
+        atoms = Atoms(positions=data.pos.numpy(), cell=cell, pbc=pbc)
+        i, j, d, D = neighbour_list("ijdD", atoms, self.cutoff)
+        data.edge_index = torch.tensor(np.array([i, j]), dtype=torch.int64)
+        data.edge_distance = torch.tensor(d, dtype=torch.float32).unsqueeze(1)
+        data.edge_vector = torch.tensor(D, dtype=torch.float32)
+        return data
